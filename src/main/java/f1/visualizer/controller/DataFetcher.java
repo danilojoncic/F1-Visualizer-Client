3eded3012d14e56dd44015415e0f82825511c503
danilojoncic <djoncic14122rn@raf.rs>
2024-02-16 03:21:44 +0100

    starting gui from scratch

--- src/main/java/f1/visualizer/controller/DataFetcher.java
+++ src/main/java/f1/visualizer/controller/DataFetcher.java
@@ -1,4 +1,4 @@
-package f1.visualizer.requests;
+package f1.visualizer.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -9,34 +9,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Requests {
-    private List<Position> positions;
-
-    public List<Position> fetchDriverLocation() {
-        positions = new ArrayList<>();
+public class DataFetcher {
+    public static List<Position> fetchDriverLocation(int driver_number){
+        List<Position> positions = new ArrayList<>();
 
         try {
-            // Specify the URL with parameters
-            String apiUrl = "http://localhost:8000/location?session_key=7953&driver_number=1&date%3E2023-03-05T15:00:00&date%3C2023-03-05T15:05:00";
-
-            // Create a URL object
+            String apiUrl = "https://api.openf1.org/v1/location?session_key=9161&driver_number=" + driver_number + "&date>2023-09-16T13:03:35.200&date<2023-09-16T13:08:35.800";
             URL url = new URL(apiUrl);
-
-            // Open a connection to the URL
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            // Set the request method to GET
             connection.setRequestMethod("GET");
-
-            // Get the response code
             int responseCode = connection.getResponseCode();
             System.out.println("Response Code: " + responseCode);
-
-            // Read the response from the API
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
             StringBuilder response = new StringBuilder();
@@ -45,19 +31,11 @@ public class Requests {
                 response.append(line);
             }
             reader.close();
-
-            // Format and print the JSON response
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode jsonNode = objectMapper.readTree(response.toString());
-
-            // Extract positions from the array
             List<Position> positionList = objectMapper.readValue(response.toString(), new TypeReference<List<Position>>() {});
-
-            // Add all positions to the existing list
             positions.addAll(positionList);
-            // Close the connection
             connection.disconnect();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -67,12 +45,5 @@ public class Requests {
         } else {
             return positions;
         }
-    }
-    public List<Position> getPositions() {
-        return positions;
-    }
-
-    public void setPositions(List<Position> positions) {
-        this.positions = positions;
     }
 }
---
+++ src/main/java/f1/visualizer/view/DrawingPanel.java
@@ -0,0 +1,14 @@
+package f1.visualizer.view;
+
+import f1.visualizer.controller.DataFetcher;
+import f1.visualizer.wrappers.Position;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrawingPanel extends JPanel {
+
+}
