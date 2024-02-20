6834066e69358c5468e6f8a184156b722edc3b45
danilojoncic <djoncic14122rn@raf.rs>
2024-02-20 20:13:54 +0100

    added new response object for getting race data and session data

--- src/main/java/f1/visualizer/wrappers/Meetings.java
+++ src/main/java/f1/visualizer/wrappers/Meetings.java
@@ -1,4 +1,18 @@
 package f1.visualizer.wrappers;
 
+import lombok.Data;
+
+@Data
 public class Meetings {
+    private String meeting_name;
+    private String meeting_name_official;
+    private String location;
+    private int country_key;
+    private String country_name;
+    private int circuit_key;
+    private String circuit_short_name;
+    private String date_start;
+    private String gmt_offset;
+    private int meeting_key;
+    private int year;
 }
