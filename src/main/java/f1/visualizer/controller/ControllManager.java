80ab7b991d1030025cf6071b5ed6a2b6cdbba511
danilojoncic <djoncic14122rn@raf.rs>
2024-02-20 02:16:41 +0100

    small fix

--- src/main/java/f1/visualizer/controller/ControllManager.java
+++ src/main/java/f1/visualizer/controller/ControllManager.java
@@ -5,7 +5,6 @@ import f1.visualizer.view.MainFrame;
 public class ControllManager {
     MainFrame mainFrame;
 
-    //ScaleController scaleController;
     PlacementController placementController;
     MotionController motionController;
     RotationController rotationController;
@@ -20,7 +19,6 @@ public class ControllManager {
         placementController = new PlacementController(mainFrame);
         debugController = new DebugController(mainFrame);
         placementController = new PlacementController(mainFrame);
-        //scaleController = new ScaleController(mainFrame);
         rotationController = new RotationController(mainFrame);
         motionController = new MotionController(mainFrame);
     }
